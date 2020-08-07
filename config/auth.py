0936065d64f26cf2aa45682bae3719206d61e4ae
Antonio Molina Dominguez <antoniomolinadominguez@MacBook-Air-de-Antonio.local>
2020-08-07 09:48:57 +0200

    Update

--- config/auth.py
+++ config/auth.py
@@ -1,4 +1,8 @@
 token = 'mytoken'                  # Token del bot
-chat_id = 'mychatID'               # Chat ID de prueba
-url = 'myurl'                      # URL del articulo
-priceLimit = 000.00                # Precio límite para notificar
+userTelegramID = 'myuserID'        # Chat ID de prueba
+
+# Defino un diccionario donde almaceno varios articulos con su url y su precio máximo
+allItems = {
+    'Frigorifico Siemens': {'priceMax': 800.0, 'url': 'https://www.mediamarkt.es/es/product/_frigor%C3%ADfico-combi-siemens-kg39nvida-36-db-no-frost-366-l-a-inox-1474566.html'}
+    'Frigorifico Balay': {'priceMax': 800.00, 'url': 'https://www.mediamarkt.es/es/product/_frigor%C3%ADfico-combi-balay-3kfe763xi-366-l-no-frost-a-inox-1474396.html'}
+    }
--- webscraper.py
+++ webscraper.py
@@ -7,35 +7,11 @@ Usage:
 Start a bot in Telegram, pass an url and a price limit and wait til bot send a notification to yousource venv/bin/activate
 """
 
-# Importo las funciones utilizadas
-import requests 
-from bs4 import BeautifulSoup
-
 # Importo la configuración del fichero
 from config.auth import *
 
-def telegram_bot_sendtext(bot_message):
-    
-    send_text = 'https://api.telegram.org/bot' + token + '/sendMessage?chat_id=' + chat_id + '&parse_mode=Markdown&text=' + bot_message
-
-    print(send_text)
-
-    requests.post(send_text)
-
 def main():
-    myUrl = requests.get(url)
-    soup = BeautifulSoup(myUrl.content, 'html.parser')
-
-    priceContent = soup.find("meta",  property="product:price:amount")
-    price = float(priceContent["content"])
-
-    if price < priceLimit:
-        message = 'El precio del articulo es ' + str(price) + ' y es menor que el precio límite indicado de ' + str(priceLimit)
-        telegram_bot_sendtext(message)
-    
-    else:
-        message = 'El precio del articulo es mayor que el precio límite indicado'
-        telegram_bot_sendtext(message)
+    print(str(len(allItems)))
 
 if __name__ == "__main__":
 	main()
