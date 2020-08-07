30a6ce9726d238b3c574422de8a70d139b73907d
Antonio Molina Dominguez <antoniomolinadominguez@MacBook-Air-de-Antonio.local>
2020-08-07 12:57:58 +0200

    Update

--- webscraper.py
+++ webscraper.py
@@ -7,11 +7,32 @@ Usage:
 Start a bot in Telegram, pass an url and a price limit and wait til bot send a notification to yousource venv/bin/activate
 """
 
+# Importo las funciones utilizadas
+import requests 
+from bs4 import BeautifulSoup
+
 # Importo la configuración del fichero
 from config.auth import *
 
+def telegram_bot_sendtext(bot_message):
+    
+    send_text = 'https://api.telegram.org/bot' + token + '/sendMessage?chat_id=' + chat_id + '&parse_mode=Markdown&text=' + bot_message
+
+    print(send_text)
+
+    requests.post(send_text)
+
 def main():
-    print(str(len(allItems)))
+
+    for item,details in allItems.items():
+        myItem = item
+        myLimit = details.get('priceMax',0)
+        myUrl = details.get('url',0)
+        print('Item: ' + myItem)
+        print('Price Limit: ' + str(myLimit))
+        print('URL: ' + str(myUrl))
+
+        
 
 if __name__ == "__main__":
 	main()
