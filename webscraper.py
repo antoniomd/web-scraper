c98e630994c807d1244e2c834dc17221dff44618
Antonio Molina Dominguez <antoniomolinadominguez@Air-de-Antonio.home>
2020-08-08 19:28:07 +0200

    Update

--- webscraper.py
+++ webscraper.py
@@ -2,53 +2,59 @@
 # -*- coding: utf-8 -*-
 
 """
-This script looks for an item in an url when the price will de less than a limit
+This script sends a notification when an item in cheaper than price maximum
 Usage:
-Start a bot in Telegram, pass an url and a price limit and wait til bot send a notification to yousource venv/bin/activate
+Pass an item, url and a price max in auth.py and wait til bot sends a notification to you
 """
 
 # Importo las funciones utilizadas
 import requests 
 from bs4 import BeautifulSoup
 
+
 # Importo la configuración del fichero
 from config.auth import *
 
-def telegram_bot_sendtext(bot_message):
-    
-    send_text = 'https://api.telegram.org/bot' + token + '/sendMessage?chat_id=' + userTelegramID + '&parse_mode=Markdown&text=' + bot_message
 
-    print(send_text)
-
-    requests.post(send_text)
-
-def main():
-
-    for item,details in allItems.items():
-        myItem = item
-        # myLimit = details.get('priceMax',0)
-        # myUrl = details.get('url',0)
-
-        # print('Item: ' + myItem)
-        # print('Price Limit: ' + str(myLimit))
-        # print('URL: ' + str(myUrl))
-
-        myUrl = requests.get(details.get('url',0))
+# Extrae el precio de la URL del articulo
+def getPrice(url):
+        myUrl = requests.get(url)
         soup = BeautifulSoup(myUrl.content, 'html.parser')
 
+        # Encuentra el precio del producto que tiene la propiedad product:price:amount
         priceContent = soup.find("meta",  property="product:price:amount")
-        price = float(priceContent["content"])
-
-        if price < details.get('priceMax',0):
-            # message = 'El precio del articulo ' + item + 'es ' + str(price) + ' y es menor que el precio límite indicado de ' + str(priceLimit)
-            telegram_bot_sendtext('El precio del articulo ' + item + ' es ' + str(price) + ' y es menor que el precio límite indicado de ' + str(details.get('priceMax',0)))
-    
+        if priceContent:
+            price = float(priceContent["content"])
+        # Si no encuentra ningún precio indica con None que no lo ha encontrado
         else:
-            # message = 'El precio del articulo ' + item + 'es mayor que el precio límite indicado'
-            telegram_bot_sendtext('El precio del articulo ' + item + ' es mayor que el precio límite indicado de ' + str(details.get('priceMax',0))')
-        
+            price = None
+        return price
 
-        
+
+def telegram_bot_sendtext(bot_message):
+    send_text = 'https://api.telegram.org/bot' + token + '/sendMessage?chat_id=' + userTelegramID + '&parse_mode=Markdown&text=' + bot_message
+    requests.post(send_text)
+
+
+def main():
+    # Recorro con un for los articulos
+    for item,details in allItems.items():
+        # item indica el nombre del artículo que estamos buscando
+        priceMax = details.get('priceMax',0)    # priceMax indica el precio máximo que estamos dispuesto a pagar
+        url = details.get('url',0)              # url indica la url del articulo
+
+        price = getPrice(url)                   # Se llama a la función getPrice donde extraigo el precio de la página
+
+        # Si no encuentro el precio en la url envío una notificación para indicarlo
+        if not price:
+            message = 'No se encuentra el precio en la url indicada'
+            telegram_bot_sendtext(message)
+
+        # Si el precio es inferior al precio máximo lo notifico
+        if price < priceMax:
+            message = 'El precio del articulo ' + item + ' es ' + str(price) + ' y es menor que el precio límite indicado de ' + str(priceMax)
+            telegram_bot_sendtext(message)
+
 
 if __name__ == "__main__":
 	main()
