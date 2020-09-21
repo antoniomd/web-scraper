9a389c4b899482b01f13744d746338727a155ed9
Antonio <64436906+antoniomd@users.noreply.github.com>
2020-09-21 16:36:59 +0200

    Update webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,23 +1,35 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 """
-rpi-monitor.py
-
-The script gets CPU temperature, use and frequency.
-
-Then these parameters can be viewed in the Terminal.
-
+This script sends a notification when an item in cheaper than price maximum
 Usage:
-Clone the repository.
-
-Run rpi-monitor.py in your RPi. Maybe, you must install psutil.
+Pass an item, url and a price max in auth.py and wait til bot sends a notification to you
+A Telegram bot and Telegram user ID is required
 """
 
-# Importo las librerias
-import subprocess, psutil, requests
+# Importo las funciones utilizadas
+import requests 
+from bs4 import BeautifulSoup
+
 
 # Importo la configuración del fichero
 from config.auth import *
+
+
+# Extrae el precio de la URL del articulo
+def getPrice(url):
+        myUrl = requests.get(url)
+        soup = BeautifulSoup(myUrl.content, 'html.parser')
+
+        # Encuentra el precio del producto que tiene la propiedad product:price:amount
+        priceContent = soup.find("meta",  property="product:price:amount")
+        if priceContent:
+            price = float(priceContent["content"])
+        # Si no encuentra ningún precio indica con None que no lo ha encontrado
+        else:
+            price = None
+        return price
 
 
 # Envía un mensaje a través de Telegram
@@ -25,39 +37,25 @@ def telegram_bot_sendtext(bot_message):
     send_text = 'https://api.telegram.org/bot' + token + '/sendMessage?chat_id=' + userTelegramID + '&parse_mode=Markdown&text=' + bot_message
     requests.post(send_text)
 
+
 def main():
-    # Obtengo la Tª de la CPU
-    tempFile = open('/sys/class/thermal/thermal_zone0/temp', 'r')
-    cpuTemp = float(tempFile.read())/1000
-    tempFile.close()
+    # Recorro con un for los articulos
+    for item,details in allItems.items():
+        # item indica el nombre del artículo que estamos buscando
+        priceMax = details.get('priceMax',0)    # priceMax indica el precio máximo que estamos dispuesto a pagar
+        url = details.get('url',0)              # url indica la url del articulo
 
-    # Obtengo el % de uso de la CPU
-    cpuUse = psutil.cpu_percent(interval=1, percpu=False)
-    
-    # Obtengo la frecuencia de la CPU
-    freqFile = open('/sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq', 'r')
-    cpuFreq = float(freqFile.read())/1000
-    freqFile.close()
-    
-    # Muestra en el Terminal la información
-    print('Temperatura CPU => ' + str(int(cpuTemp)) + ' ºC')
-    print('Uso de la CPU => ' + str(cpuUse) + ' %')
-    print('Frecuencia CPU => ' + str(cpuFreq) + ' Hz')
+        price = getPrice(url)                   # Se llama a la función getPrice donde extraigo el precio de la página
 
-    # Gestión alarmas de temperatura
-    if cpuTemp >= 80 and cpuTemp < 85:
-        message = 'La temperatura de la CPU de nuestra Raspberry ha alcanzado los 80ºC'
-        if warningTemp is False:
+        # Si no encuentro el precio en la url envío una notificación para indicarlo
+        if price == None:
+            message = 'No se encuentra el precio en la url indicada'
             telegram_bot_sendtext(message)
-            warningTemp = True
-    elif cpuTemp >= 85:
-        message = 'La temperatura de la CPU de nuestra Raspberry ha alcanzado los 85ºC'
-        if tripTemp is False:
-            telegram_bot_sendtext(message)
-            tripTemp = True
-    else:
-        warningTemp = tripTemp = False
 
+        # Si el precio es inferior al precio máximo lo notifico
+        elif price < priceMax:
+            message = 'El precio del articulo ' + item + ' es ' + str(price) + ' y es menor que el precio límite indicado de ' + str(priceMax)
+            telegram_bot_sendtext(message)
 
 
 if __name__ == "__main__":
